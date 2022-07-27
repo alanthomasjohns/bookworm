48a0367642c0e0e6c7346d8a6b63cb794f683ca2
Alan Johns <alanjohns@pop-os.localdomain>
2022-07-27 20:33:29 +0530

    updated .env file

--- books_com/settings.py
+++ books_com/settings.py
@@ -89,11 +89,11 @@ AUTH_USER_MODEL = 'accounts.Account'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'books',
-        'USER': 'postgres',
-        'PASSWORD': 'trial1222',
-        'HOST': 'database-2.ceen8zjlllsl.us-east-1.rds.amazonaws.com'
+        'ENGINE': config('ENGINE'),
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST': config('HOST')
     }
 }
 
@@ -141,16 +141,16 @@ USE_TZ = True
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_FILE_OVERWRITE = False
 
-AWS_ACCESS_KEY_ID = 'AKIA2OCL72DO6UDFON65'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'bookwormdjango-bucket'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = config('AWS_S3_CUSTOM_DOMAIN')
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl' : 'max-age=86400',
 }
 
 AWS_DEFAULT_ACL = None
-AWS_LOCATION = 'static'
+AWS_LOCATION = config('AWS_LOCATION')
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
